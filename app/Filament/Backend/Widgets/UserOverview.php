8a7613426b21c4f2291e20e2410b8d1c59e35b18
Jason <20511924@qq.com>
2025-03-15 22:15:19 +0800

    清理

--- app/Filament/Backend/Widgets/UserOverview.php
+++ app/Filament/Backend/Widgets/UserOverview.php
@@ -10,13 +10,6 @@ use App\Models\User;
 use Carbon\Carbon;
 use Filament\Widgets\StatsOverviewWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
-use Modules\Mall\Enums\OrderStatus;
-use Modules\Mall\Filament\Resources\OrderResource;
-use Modules\Mall\Models\Order;
-use Modules\Payment\Enums\PaymentStatus;
-use Modules\Payment\Filament\Resources\PaymentResource;
-use Modules\Payment\Models\Payment;
-use Nwidart\Modules\Facades\Module;
 
 class UserOverview extends StatsOverviewWidget
 {
@@ -24,7 +17,7 @@ class UserOverview extends StatsOverviewWidget
 
     protected function getStats(): array
     {
-        $states = [
+        return [
             Stat::make('用户数量', User::count())
                 ->description('今日新增：'.User::whereDate('created_at', Carbon::today())->count())
                 ->descriptionIcon('heroicon-m-arrow-trending-up')
@@ -36,23 +29,5 @@ class UserOverview extends StatsOverviewWidget
                 ->color('success')
                 ->url(ExamineResource::getUrl()),
         ];
-
-        if (Module::has('Mall') && Module::isEnabled('Mall')) {
-            $states[] = Stat::make('待发货订单', Order::where('status', OrderStatus::Paid)->count())
-                ->description('今日订单：'.Order::whereDate('created_at', Carbon::today())->count())
-                ->descriptionIcon('heroicon-m-arrow-trending-up')
-                ->color('success')
-                ->url(OrderResource::getUrl());
-        }
-
-        if (Module::has('Payment') && Module::isEnabled('Payment')) {
-            $states[] = Stat::make('今日收款', Payment::where('status', PaymentStatus::Paid)->sum('amount'))
-                ->description('收款订单：'.Payment::whereDate('created_at', Carbon::today())->count())
-                ->descriptionIcon('heroicon-m-arrow-trending-up')
-                ->color('success')
-                ->url(PaymentResource::getUrl());
-        }
-
-        return $states;
     }
 }
