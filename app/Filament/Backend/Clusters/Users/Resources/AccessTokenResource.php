efee2ec94888a922dd43c8640d3923f32ed73bc8
Jason <20511924@qq.com>
2025-03-15 20:58:03 +0800

    凭证权限完善

--- app/Filament/Backend/Clusters/Users/Resources/AccessTokenResource.php
+++ app/Filament/Backend/Clusters/Users/Resources/AccessTokenResource.php
@@ -3,8 +3,11 @@
 namespace App\Filament\Backend\Clusters\Users\Resources;
 
 use App\Filament\Backend\Clusters\Users;
-use App\Filament\Backend\Clusters\Users\Resources\AccessTokenResource\Pages\ManageAccessTokens;
+use App\Filament\Backend\Clusters\Users\Resources\AccessTokenResource\Pages;
 use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
 use Laravel\Sanctum\PersonalAccessToken;
 
 class AccessTokenResource extends Resource
@@ -21,10 +24,36 @@ class AccessTokenResource extends Resource
 
     protected static ?string $cluster = Users::class;
 
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->modifyQueryUsing(fn(Builder $query) => $query->orderByDesc('last_used_at'))
+            ->columns([
+                Tables\Columns\TextColumn::make('tokenable.username')
+                    ->label('用户')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('name')
+                    ->label('名称'),
+                Tables\Columns\TextColumn::make('abilities')
+                    ->label('权限'),
+                Tables\Columns\TextColumn::make('last_used_at')
+                    ->label('最后使用时间'),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->translateLabel(),
+            ])
+            ->actions([
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
     public static function getPages(): array
     {
         return [
-            'index' => ManageAccessTokens::route('/'),
+            'index' => Pages\ManageAccessTokens::route('/'),
         ];
     }
 }
