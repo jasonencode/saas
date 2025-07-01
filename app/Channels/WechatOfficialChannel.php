f1a12b1042864a4a4cab5510ca908aede8bd3339
Jason.Chen <chenjxlg@163.com>
2025-07-01 14:19:23 +0800

    微信模板消息demo

--- app/Channels/WechatOfficialChannel.php
+++ app/Channels/WechatOfficialChannel.php
@@ -3,7 +3,9 @@
 namespace App\Channels;
 
 use App\Contracts\Authenticatable;
+use App\Contracts\WechatOfficialMessage;
 use Illuminate\Notifications\Notification;
+use Overtrue\LaravelWeChat\EasyWeChat;
 
 /**
  * 微信公众号通知通道
@@ -13,7 +15,23 @@ class WechatOfficialChannel
     public function send(Authenticatable $user, Notification $notification): void
     {
         if (method_exists($notification, 'toWechatOfficial')) {
+            /** @var WechatOfficialMessage $message */
             $message = $notification->toWechatOfficial($user);
+
+            if (!$message) {
+                return;
+            }
+
+            $app = EasyWeChat::officialAccount();
+
+            $app->getClient()->postJson('cgi-bin/message/template/send', [
+                'touser' => $message->getOpenId(),
+                'template_id' => $message->getTemplateId(),
+                'url' => $message->getUrl(),
+                'miniprogram' => $message->getMiniprogram(),
+                'data' => $message->getData(),
+                'client_msg_id' => time(),
+            ]);
         }
     }
 }
--- app/Contracts/WechatOfficialMessage.php
+++ app/Contracts/WechatOfficialMessage.php
@@ -2,9 +2,97 @@
 
 namespace App\Contracts;
 
+/**
+ * 数据库消息
+ *
+ * 使用示例:
+ *  WechatOfficialMessage::make()
+ *     ->openId($notifiable->open_id)
+ *     ->templateId('H3n7xB2C0U_RgO1fzsyxI1WZ6KKlM6qmVEuCc6n55W0')
+ *     ->url('https://cjango.com')
+ *     ->payload('User', 'Jason')
+ *     ->payload('Date', now())
+ *     ->payload('Status', '已完成');
+ */
 class WechatOfficialMessage
 {
-    public function __construct()
+    protected string $template_id;
+
+    protected string $openId;
+
+    protected ?string $url = null;
+
+    protected array $data = [];
+
+    protected array $miniprogram = [];
+
+    public static function make(): WechatOfficialMessage
     {
+        return new self();
+    }
+
+    public function templateId(string $templateId): static
+    {
+        $this->template_id = $templateId;
+
+        return $this;
+    }
+
+    public function openId(string $openId): static
+    {
+        $this->openId = $openId;
+
+        return $this;
+    }
+
+    public function url(?string $url): static
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    public function miniprogram(string $appid, string $pagepath): static
+    {
+        $this->miniprogram = [
+            'appid' => $appid,
+            'pagepath' => $pagepath,
+        ];
+
+        return $this;
+    }
+
+    public function payload(string $key, string $value): static
+    {
+        $this->data[$key] = [
+            'value' => $value,
+        ];
+
+        return $this;
+    }
+
+    public function getTemplateId(): string
+    {
+        return $this->template_id;
+    }
+
+    public function getOpenId(): string
+    {
+        return $this->openId;
+    }
+
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+    public function getData(): array
+    {
+        return $this->data;
+    }
+
+    public function getMiniprogram(): array
+    {
+        return $this->miniprogram;
     }
 }
