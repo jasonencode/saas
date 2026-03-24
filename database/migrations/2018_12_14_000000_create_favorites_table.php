78a872195ffd8a6b3e454f5b8f8987e2266bb013
Jason <20511924@qq.com>
2026-03-24 22:13:56 +0800

    feat(favorite): 增加favorite扩展

---
+++ database/migrations/2018_12_14_000000_create_favorites_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create(config('favorite.favorites_table'), static function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger(config('favorite.user_foreign_key'))
+                ->index()
+                ->comment('user_id');
+            $table->morphs('favoriteable');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists(config('favorite.favorites_table'));
+    }
+};
