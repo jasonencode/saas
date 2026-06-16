7a060c62d764becc41d086847e3df9b63ffca0f4
Jason.Chen <chenjxlg@163.com>
2026-06-16 14:33:27 +0800

    feat(finance): 添加发票抬头策略权限控制

---
+++ app/Policies/Finance/InvoiceTitlePolicy.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Policies\Finance;
+
+use App\Contracts\Authenticatable;
+use App\Contracts\Policy;
+use App\Contracts\PolicyName;
+
+class InvoiceTitlePolicy extends Policy
+{
+    protected string $modelName = '发票抬头';
+
+    protected string $groupName = '财务管理';
+
+    #[PolicyName('列表', '')]
+    public function viewAny(Authenticatable $user): bool
+    {
+        return $user->hasPermission(__CLASS__, __FUNCTION__);
+    }
+
+    #[PolicyName('详情', '')]
+    public function view(Authenticatable $user): bool
+    {
+        return $user->hasPermission(__CLASS__, __FUNCTION__);
+    }
+
+    #[PolicyName('创建', '')]
+    public function create(Authenticatable $user): bool
+    {
+        return $user->hasPermission(__CLASS__, __FUNCTION__);
+    }
+
+    #[PolicyName('编辑', '')]
+    public function update(Authenticatable $user): bool
+    {
+        return $user->hasPermission(__CLASS__, __FUNCTION__);
+    }
+
+    #[PolicyName('删除', '')]
+    public function delete(Authenticatable $user): bool
+    {
+        return $user->hasPermission(__CLASS__, __FUNCTION__);
+    }
+
+    #[PolicyName('批量删除')]
+    public function deleteAny(Authenticatable $user): bool
+    {
+        return $user->hasPermission(__CLASS__, __FUNCTION__);
+    }
+
+    #[PolicyName('设置默认')]
+    public function setDefault(Authenticatable $user): bool
+    {
+        return $user->hasPermission(__CLASS__, __FUNCTION__);
+    }
+}
