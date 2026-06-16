3e7b4ee5b4f2e8167009c51131187cd539f5b3f0
Jason.Chen <chenjxlg@163.com>
2026-06-16 15:34:04 +0800

    refactor(system): 优化系统配置和数据缓存

---
+++ app/Filament/Backend/Clusters/Setting/Resources/Imports/Pages/ManageImports.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Backend\Clusters\Setting\Resources\Imports\Pages;
+
+use App\Filament\Backend\Clusters\Setting\Resources\Imports\ImportResource;
+use Filament\Resources\Pages\ManageRecords;
+
+class ManageImports extends ManageRecords
+{
+    protected static string $resource = ImportResource::class;
+}
