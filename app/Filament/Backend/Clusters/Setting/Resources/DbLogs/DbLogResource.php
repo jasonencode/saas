ff7bbd505f11d8cb6fc454376099cc344351a8f1
Jason.Chen <chenjxlg@163.com>
2026-08-05 17:14:29 +0800

    refactor(DbLogs): 重构数据库日志资源配置

--- app/Filament/Backend/Clusters/Setting/Resources/DbLogs/DbLogResource.php
+++ app/Filament/Backend/Clusters/Setting/Resources/DbLogs/DbLogResource.php
@@ -2,58 +2,42 @@
 
 namespace App\Filament\Backend\Clusters\Setting\Resources\DbLogs;
 
-use App\Filament\Backend\Clusters\Setting\Resources\DbLogs\Pages\CreateDbLog;
-use App\Filament\Backend\Clusters\Setting\Resources\DbLogs\Pages\EditDbLog;
-use App\Filament\Backend\Clusters\Setting\Resources\DbLogs\Pages\ManageDbLogs;
-use App\Filament\Backend\Clusters\Setting\Resources\DbLogs\Pages\ViewDbLog;
-use App\Filament\Backend\Clusters\Setting\Resources\DbLogs\Schemas\DbLogForm;
-use App\Filament\Backend\Clusters\Setting\Resources\DbLogs\Schemas\DbLogInfolist;
-use App\Filament\Backend\Clusters\Setting\Resources\DbLogs\Tables\DbLogsTable;
 use App\Filament\Backend\Clusters\Setting\SettingCluster;
 use App\Models\System\DBLog;
-use BackedEnum;
 use Filament\Resources\Resource;
 use Filament\Schemas\Schema;
-use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Table;
+use UnitEnum;
 
 class DbLogResource extends Resource
 {
     protected static ?string $model = DBLog::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
-
     protected static ?string $cluster = SettingCluster::class;
 
-    public static function form(Schema $schema): Schema
-    {
-        return DbLogForm::configure($schema);
-    }
+    protected static ?string $modelLabel = '日志';
+
+    protected static ?string $navigationLabel = '系统日志';
+
+    protected static string|null|UnitEnum $navigationGroup = '维护';
+
+    protected static ?int $navigationSort = 100;
 
     public static function infolist(Schema $schema): Schema
     {
-        return DbLogInfolist::configure($schema);
+        return Schemas\DbLogInfolist::configure($schema);
     }
 
     public static function table(Table $table): Table
     {
-        return DbLogsTable::configure($table);
-    }
-
-    public static function getRelations(): array
-    {
-        return [
-            //
-        ];
+        return Tables\DbLogsTable::configure($table);
     }
 
     public static function getPages(): array
     {
         return [
-            'index' => ManageDbLogs::route('/'),
-            'create' => CreateDbLog::route('/create'),
-            'view' => ViewDbLog::route('/{record}'),
-            'edit' => EditDbLog::route('/{record}/edit'),
+            'index' => Pages\ManageDbLogs::route('/'),
+            'view' => Pages\ViewDbLog::route('/{record}'),
         ];
     }
 }
